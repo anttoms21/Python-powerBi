feea953fa19d64610d359c3a0c49bf699d17812b
Thomas Tran <46790549+anttoms21@users.noreply.github.com>
2019-06-04 10:59:32 -0400

    COmpleted exporting the data to excel, working on getting the table to take in both GJ/yr and GJ_per_m2

--- Creating_excel_tables.py
+++ Creating_excel_tables.py
@@ -16,12 +16,12 @@ class TableMaker():
     BUILDING_TYPE = 'building_type'
     CITY = 'geography.city'
     TEMPLATE = 'template'
-    TOTAL_END_USES_GJ_PER_M2 = None
+    #TOTAL_END_USES_GJ_PER_M2 = None
     TOTAL_END_USES_GJ_PER_YEAR = None
 
-    def __init__(self, desired_values_gj_per_year, desired_values_gj_per_m2,
+    def __init__(self, desired_values_gj_per_year, #desired_values_gj_per_m2,
                  url='C:/Users/thotran/Desktop/simulations.json'):
-        self.TOTAL_END_USES_GJ_PER_M2 = desired_values_gj_per_m2
+       # self.TOTAL_END_USES_GJ_PER_M2 = desired_values_gj_per_m2
         self.TOTAL_END_USES_GJ_PER_YEAR = desired_values_gj_per_year
         pd.set_option('display.max_columns', None)
         with open(url) as f:
@@ -35,7 +35,7 @@ class TableMaker():
         self.main_table.drop(self.main_table[(~self.main_table[column].isin(values))].index, inplace=True)
         return self
 
-    def necb_comparison_table(self, columns=[TOTAL_END_USES_GJ_PER_YEAR, TOTAL_END_USES_GJ_PER_M2],
+    def necb_comparison_table(self, columns=[TOTAL_END_USES_GJ_PER_YEAR],# TOTAL_END_USES_GJ_PER_M2],
                               templates=['NECB2015', 'NECB2017']):
         # https://stackoverflow.com/questions/35268817/unique-combinations-of-values-in-selected-columns-in-pandas-data-frame-and-count
         # Create a table just with cities and building types. Keeping 'count' for now for debugging.
@@ -55,8 +55,8 @@ class TableMaker():
         return table
 
 
-def creating_tables(gj_per_year, gj_per_m2):
-    tm = TableMaker(gj_per_year, gj_per_m2)
+def creating_tables(gj_per_year):
+    tm = TableMaker(gj_per_year)
     # filters by cities you want
     tm.filter_out_rows_by_values(tm.BUILDING_TYPE, ['SmallOffice',
                                                     'MediumOffice',
@@ -73,25 +73,36 @@ def creating_tables(gj_per_year, gj_per_m2):
     # This creates a table with necb201x values in columns rather than rows.
     # the first arguement is the column from the simulations.json top level down, you can add more as you need.. the second is the vintages you want in the table.
     # You can have a single vintage if you want to have a table like 4.3.3.a
-    comparison_table = tm.necb_comparison_table([f"{TOTAL_END_USES_GJ_PER_YEAR}"_, f"{TOTAL_END_USES_GJ_PER_M2}"],
+    comparison_table = tm.necb_comparison_table([gj_per_year],
                                                 ['NECB2015', 'NECB2017'])
     # You should be able to rename the columns after the table is created to be pretty.
     return comparison_table
 
 
-wanted_values_gj = []
-wanted_values_gj_per_m2 = []
+wanted_values_gj = ['end_uses.heating_gj','end_uses.cooling_gj']
+wanted_values_gj_per_m2 = ['end_uses_eui.heating_gj_per_m2', 'end_uses_eui.cooling_gj_per_m2']
 arr = []
 if (len(wanted_values_gj) != len(wanted_values_gj_per_m2)):
     raise ('please make sure that the two arrays are equal in length')
 for i in range(0, len(wanted_values_gj)):
-    arr[i] = creating_tables(wanted_values_gj[i],wanted_values_gj_per_m2[i])
+    print(len(wanted_values_gj_per_m2))
+    print (wanted_values_gj[i])
+    #raise('hell')
+    arr.append(creating_tables(wanted_values_gj[i]))#, wanted_values_gj_per_m2[i])
 
-raise ('hell')
+#raise ('hell')
 # this already works I am testing if the current for loop above will work
 output = pd.ExcelWriter('C:/Users/thotran/Desktop/panda_simple.xlsx', engine='xlsxwriter')
 my_file = Path('C:/Users/thotran/Desktop/panda_simple.xlsx')
 if my_file.is_file():
     os.remove('C:/Users/thotran/Desktop/panda_simple.xlsx')
-comparison_table.to_excel(output, sheet_name='NECB2011')
+for x in range(0,len(arr)):
+    name = f"{wanted_values_gj[x]}"
+    z= name.split('_')
+    #print(z)
+
+    Name2 =z[1].split('.')
+    sheetName = Name2[1]
+    data = arr[x]
+    data.to_excel(output, sheet_name=sheetName)
 output.close()
